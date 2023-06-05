c99902b62ff6d72ff6975ccb048b151d1c19c209
Edi Javier <126507385+edijavier99@users.noreply.github.com>
2023-06-05 18:15:34 +0000

    changes done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [likes, setLikes ] = useState(0)
+
+	let addLikes = () =>{
+		setLikes(prevCont => prevCont +1)
+	}
+	let noLikes = () =>{
+		setLikes(prevCont => prevCont - 1)
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+			<div className=" d-flex justify-content-center align-items-center vh-100">
+				<div className="row text-center">
+					<p> {likes} likes </p>
+						<div className="mb-3">
+							<i className="fas fa-thumbs-up me-3" onClick={addLikes}></i>
+							<i className="fas fa-thumbs-down" onClick={noLikes} ></i>
+						</div>
+					<p>Like or dislike to increase/ decrease</p>
+				</div>	
+			</div>
 	);
 };
 
